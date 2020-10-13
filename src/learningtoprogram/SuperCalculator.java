d87f20cd59f3856d775f228f463b35db55741855
fredd <fredd@192.168.2.11>
2020-10-13 11:18:01 -0400

    I added all the colours of the rainbow to the drawing in SuperCalculator and added comments inside it.

--- src/learningtoprogram/SuperCalculator.java
+++ src/learningtoprogram/SuperCalculator.java
@@ -2,7 +2,7 @@
  * Author: Frederick Yu
  * Class: ICS3U
  *
- * Program: ASCII Art
+ * Program: SuperCalculator
  * Input: I wil be inputting a mixture of symbols to create some sort of art.
  * Output: Some sort of art will be outputted.
 
@@ -29,36 +29,36 @@ public class SuperCalculator{
     public static void main(String[] args) {
         Console c = new Console();
         c.setCursor(9,40);
-        c.setTextColor(Color.RED);
+        c.setTextColor(Color.RED);//This changes the colour to red
         c.println("   ---   "); //I am trying to create a square here
         c.setCursor(10,40);
-        c.setTextColor(Color.ORANGE);
+        c.setTextColor(Color.ORANGE);//This changes the colour to orange
         c.println("   | |   ");
         c.setCursor(11,40);
-        c.setTextColor(Color.YELLOW);
+        c.setTextColor(Color.YELLOW);//This changes the colour to yellow
         c.println("   ---   ");//This is the end of my square
         c.setCursor(12,40);
-        c.setTextColor(Color.GREEN);
+        c.setTextColor(Color.GREEN);//This changes the colour to green
         c.println("----------");//This is the beginning of me trying to draw the letter F
         c.setCursor(13,40);
-        c.setTextColor(Color.CYAN);
+        c.setTextColor(Color.CYAN);//This changes the colour to cyan, which is basically light blue
         c.println("|");
         c.setCursor(14,40);
-        c.setTextColor(Color.BLUE);
+        c.setTextColor(Color.BLUE);//This changes the colour to blue
         c.println("|");
         c.setCursor(15,40);
-        c.setTextColor(Color.MAGENTA);
+        c.setTextColor(Color.MAGENTA);//This changes the colour to magenta
         c.println("|------");
         c.setCursor(16,40);
-        c.setTextColor(Color.RED);
+        c.setTextColor(Color.RED);//This changes the colour to red
         c.println("|");
         c.setCursor(17,40);
-        c.setTextColor(Color.ORANGE);
+        c.setTextColor(Color.ORANGE);//This changes the colour to orange
         c.println("|");
         c.setCursor(18,40);
-        c.setTextColor(Color.YELLOW);
+        c.setTextColor(Color.YELLOW);//This changes the colour to yellow
         c.println("|");
-        c.setTextColor(Color.BLACK);
+        c.setTextColor(Color.BLACK);//I am changing the colour back to black for the text, so the user can read the text more easily.
         String enter;//I am creating a string that allows the user to type something to clear the screen
         c.println("If you would like to clear the screen please enter a letter");
         enter = c.readLine();//This allows the user to type something
