6792dbfbb8dbcb81c7e2f5bb28933f1cce638c81
fredd <fredd@192.168.2.11>
2020-10-12 20:53:53 -0400

    I did all of assignment 3, except changing the colour, because it says that cannot find symbol.

--- src/learningtoprogram/RestaurantHelper.java
+++ src/learningtoprogram/RestaurantHelper.java
@@ -34,6 +34,15 @@ public class RestaurantHelper{
         c.printDate();//I am telling them the date
         c.println(" ");//I want to seperate the date and time, so it's not in a cluster
         c.printTime();//I am printing the time 
+        c.println(" ");
+        double billTotal;//I am setting up a variable that I can use later to store the bill
+        c.println("I hope you had a great evening what would your bill total be today?");//This is providing the user the qeustion to how much they will be paying
+        billTotal = c.readDouble();//This let's the program read what the user says.
+        double tip;//I am creating a variable for how much tip they want to give.
+        c.println("How much would you like to tip");//I am asking how much tip they are willing to pay
+        tip = c.readDouble();//This let's the program to read how much tip they are paying
+        double finalCost = tip + billTotal;//This variable calculates the total cost of everything combined by add the billTotal and the tip.
+        c.println("Great. Your total today will be " + "$" + finalCost);//Here I am tellilng the user how my much they need to pay.
     }
 
 }
--- src/learningtoprogram/SuperCalculator.java
+++ src/learningtoprogram/SuperCalculator.java
@@ -26,17 +26,41 @@ public class SuperCalculator{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Console c = new Console();			
+        Console c = new Console();
+        c.setCursor(9,40);
+//        c.setTextColor(Color.RED);
         c.println("   ---   "); //I am trying to create a square here
+        c.setCursor(10,40);
         c.println("   | |   ");
+        c.setCursor(11,40);
         c.println("   ---   ");//This is the end of my square
+        c.setCursor(12,40);
         c.println("----------");//This is the beginning of me trying to draw the letter F
+        c.setCursor(13,40);
         c.println("|");
+        c.setCursor(14,40);
         c.println("|");
+        c.setCursor(15,40);
         c.println("|------");
+        c.setCursor(16,40);
         c.println("|");
+        c.setCursor(17,40);
         c.println("|");
+        c.setCursor(18,40);
         c.println("|");
+        String enter;//I am creating a string that allows the user to type something to clear the screen
+        c.println("If you would like to clear the screen please enter a letter");
+        enter = c.readLine();//This allows the user to type something
+        c.clear();//This clears the screens
+        int menu;//I am creating a variable for the user to enter the number the mathces their option
+        c.println("Now there will be a menu with 6 options. Type the number next to the option to select it");
+        c.println("1) Linear Equations");
+        c.println("2) Boolean Logic");
+        c.println("3) Number Fun");
+        c.println("4) Word Math");
+        c.println("5) Quadratic Equations");
+        c.println("6) Statistics");
+        menu = c.readInt();//This reads which option the user chooses.
     }
 
 }
