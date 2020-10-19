c7c51b658fa82552a13bc0d47dba18d117e622a3
fredd <fredd@192.168.2.11>
2020-10-19 11:44:06 -0400

    I finished assignment 4 and add all the comments. I also did the rounding. Lastly, I added if for super calculator.

--- src/learningtoprogram/SuperCalculator.java
+++ src/learningtoprogram/SuperCalculator.java
@@ -16,13 +16,17 @@ import hsa.*;
 import boardGame.*;
 import becker.robots.*;
 import java.awt.Color;
+import static learningtoprogram.SuperCalculator.roundToDecimals;
 
 /**
  *
  * @author Frederick Yu
  */
 public class SuperCalculator{
-
+public static double roundToDecimals(double d, int c) {
+int temp=(int)((d*Math.pow(10,c)));
+return (((double)temp)/Math.pow(10,c));
+}
     /**
      * @param args the command line arguments
      */
@@ -76,6 +80,81 @@ c.println("4) Word Math");
 c.println("5) Quadratic Equations");
 c.println("6) Statistics");
 menu = c.readInt();//This reads which option the user chooses.
+//Next I will be creating if statements that tells the program to change the screen depending on which number the user enters
+if(menu == 1){
+   c.clear();
+   c.println("Great you have chosen Linear Equations!");
+   double x1;//I am creating a variable to store the point x1
+   double y1;//I am creating a variable to store the point y1
+   double x2;//I am creating a variable to store the point x2
+   double y2;//I am creating a variable to store the point y2
+   //Asking and sotring the variables for the following problems
+   c.println("What number will x1 be?");
+   x1 = c.readDouble();
+   c.println("What number will y1 be?");
+   y1 = c.readDouble();
+   c.println("What number will x2 be?");
+   x2 = c.readDouble();
+   c.println("What number will y2 be?");
+   y2 = c.readDouble();
+   //Telling the user what they have entered
+   c.println("Equations for points (" + x1 + "," + y1 + ") (" + x2 + "," + y2 + ")");
+   //Creating a variable to store the distance
+   //I first look at the equation and then I just put in my vairables. 
+   double distance = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+   //Then I round the answer to 2 decimal places.
+   distance = roundToDecimals(distance,2);
+   //I print out the answer that has been rounded
+   c.println("Distance between Points: " + distance);
+   //Now I am going to find the slope with the point that have provided.
+   double slope;
+   slope = (y1 - y2) / (x1 - x2);
+   //I am rounding the answer and then printing it out for the user
+   slope = roundToDecimals(slope,2);
+   c.println("Slope of Line: The slope of the given line is " + slope);
+   //Now I am going to find the equation of the line with the points given. I first tell the user what the equation will be.
+   c.println("The equation of a line is y = mx+b");
+   double b = y1 - slope * x1;
+   //I am rounding b for when I output it.
+   b = roundToDecimals(b,2);
+   double equationOfLine = y1 = (slope * x1) + b;
+   equationOfLine = roundToDecimals(equationOfLine,2);
+   //Here I am only outputting the slope and b as I am only giving the equation
+   c.println("Equation of Line: The equation of the line with the given points is y = " + slope + "x + " + b);
+   //Next I will be finding the midpoint. I created two variables, one that stores x and the other that stores y
+   double midpointX = (x1 + x2) / 2;
+   double midpointY = (y1 + y2) / 2;
+   //Now I have to round both and out put it
+   midpointX = roundToDecimals(midpointX,2);
+   midpointY = roundToDecimals(midpointY,2);
+   c.println("Midpoint: The midpoint of the line segment is: (" + midpointX + "," + midpointY + ")");
     }
-
+else if(menu == 2){
+    c.clear();
+    c.println("Great you have chosen Boolean Logic!");
+    c.println("Sorry this is still under construction");
+    }
+else if(menu == 3){
+    c.clear();
+    c.println("Great you have chosen Number Fun!");
+    c.println("Sorry this is still under construction");
+    }
+else if(menu == 4){
+    c.clear();
+    c.println("Great you have chosen Word Math!");
+    c.println("Sorry this is still under construction");
+    }
+else if(menu == 5){
+    c.clear();
+    c.println("Great you have chosen Quadratic Equations!");
+    c.println("Sorry this is still under construction");
+    }   
+else if(menu == 6){
+    c.clear();
+    c.println("Great you have chosen Satistics!");
+    c.println("Sorry this is still under construction");
+    }
+  }
 }
+
+
